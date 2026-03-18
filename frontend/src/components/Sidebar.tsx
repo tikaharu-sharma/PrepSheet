86f54e51d49e378e549ea95f6ee5c075c8086390
Tikaharu Sharma <ashok@MacBookPro.attlocal.net>
2026-03-18 11:30:11 -0400

    Implemented restaurant selector in sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -9,7 +9,10 @@ import {
   Avatar,
   IconButton,
   useTheme,
-  useMediaQuery
+  useMediaQuery,
+  Select,
+  MenuItem,
+  FormControl
 } from "@mui/material";
 
 import HomeIcon from "@mui/icons-material/Home";
@@ -22,14 +25,15 @@ import InsightsIcon from "@mui/icons-material/Insights";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import LogoutIcon from "@mui/icons-material/Logout";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 import { useNavigate, useLocation } from 'react-router-dom'
 import { logoutMock } from '../lib/auth'
+import { getRestaurants, type Restaurant } from '../pages/Restaurants'
 
 import PrepSheetLogo from "../assets/PrepSheet.svg";
 
 
+import { useState, useEffect } from "react";
 const drawerWidth = 260;
 const activeColor = "#4ea674";
 
@@ -50,10 +54,26 @@ export default function Sidebar({ mobileOpen, handleDrawerToggle }: SidebarProps
   const navigate = useNavigate()
   const location = useLocation()
 
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([])
+  const [selectedRestaurant, setSelectedRestaurant] = useState<string>('')
+
+  useEffect(() => {
+    const loadedRestaurants = getRestaurants()
+    setRestaurants(loadedRestaurants) // eslint-disable-line react-hooks/set-state-in-effect
+    // Set default to first restaurant if available
+    if (loadedRestaurants.length > 0 && !selectedRestaurant) {
+      setSelectedRestaurant(loadedRestaurants[0].id)
+    }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
 	const handleLogout = () => {
 		logoutMock()
 		navigate('/login')
 	}
+
+  const handleLogoClick = () => {
+    navigate('/home')
+  }
 
 
 
@@ -113,22 +133,47 @@ const getItemStyle = (path: string) => ({
       {/* TOP SECTION*/}
       <Box>
         {/* LOGO */}
-        <Box sx={{ mb: 3 }}>
+        <Box sx={{ mb: 3, cursor: 'pointer' }} onClick={handleLogoClick}>
           <img src={PrepSheetLogo} alt="PrepSheet Logo" style={{ width: 140}} />
         </Box>
 
         {/* RESTAURANT SELECTOR */}
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            mb: 2
-          }}
-        >
-          <Typography>Restaurant A</Typography>
-          <KeyboardArrowDownIcon />
-        </Box>
+        <FormControl fullWidth sx={{ mb: 2 }}>
+          <Select
+            value={selectedRestaurant}
+            onChange={(e) => setSelectedRestaurant(e.target.value)}
+            displayEmpty
+            sx={{
+              '& .MuiSelect-select': {
+                display: 'flex',
+                alignItems: 'center',
+                py: 1,
+                fontSize: '0.8rem',
+                lineHeight: 1.2,
+                whiteSpace: 'normal',
+                wordBreak: 'break-word',
+                minHeight: 'auto',
+              }
+            }}
+            MenuProps={{
+              PaperProps: {
+                sx: {
+                  maxHeight: 300,
+                  '& .MuiMenuItem-root': {
+                    whiteSpace: 'normal',
+                    wordWrap: 'break-word',
+                  }
+                }
+              }
+            }}
+          >
+            {restaurants.map((restaurant) => (
+              <MenuItem key={restaurant.id} value={restaurant.id}>
+                {restaurant.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
         {/* MAIN MENU */}
         <List>
