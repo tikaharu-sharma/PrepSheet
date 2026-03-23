f9705f1ab5c91e1a44c992521a0c719e2cccb201
Sampada Sharma <sampadaasfren@gmail.com>
2026-03-23 13:17:49 -0400

    Set Cypress baseUrl to localhost:5173 for consistent test execution

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: "http://localhost:5174", //frontend dev server
+    baseUrl: "http://localhost:5173", //frontend dev server
     specPattern: "cypress/e2e/**/*.spec.ts",
     setupNodeEvents(on, config) {
       // implement node event listeners here
